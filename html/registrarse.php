53f23c632b879c21dc810031ba5cfa767d51ab7b
MaykelRobles <82823403+MaykelRobles@users.noreply.github.com>
2021-05-28 20:08:02 -0600

    php
    update

--- html/registrarse.php
+++ html/registrarse.php
@@ -89,7 +89,7 @@
                         </div>
 
                         <div class="col">
-                            <label type="submit" class="numPer numPerPad" placeholder="0"></label>
+                            <label type="submit" class="numPer numPerPad" id="number"></label>
                         </div>
 
                         <div class="col-1">
@@ -97,7 +97,7 @@
                         </div>
 
                         <div class="col">
-                            <label type="submit" class="numPer numPerPad" placeholder="0"></label>
+                            <label type="submit" class="numPer numPerPad" id="number"></label>
                         </div>
                     </div>
                 </div>
@@ -116,7 +116,7 @@
                         </div>
 
                         <div class="col">
-                            <label type="submit" class="numPer numPerPad" placeholder="0"></label>
+                            <label type="submit" class="numPer numPerPad" id="number"></label>
                         </div>
 
                         <div class="col-1">
@@ -124,7 +124,7 @@
                         </div>
 
                         <div class="col">
-                            <label type="submit" class="numPer numPerPad"  placeholder="0"></label>
+                            <label type="submit" class="numPer numPerPad"  id="number"></label>
                         </div>
                     </div>
                 </div>
