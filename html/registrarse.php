4be335282da71e32b09e683a9c3e1c5a40c320b9
MaykelRobles <82823403+MaykelRobles@users.noreply.github.com>
2021-05-28 20:51:03 -0600

    php
    update

--- html/registrarse.php
+++ html/registrarse.php
@@ -17,7 +17,7 @@
 
 <nav class="navbar navbar-expand-lg navColor">
     <div class="container-fluid">
-        <a href="../index.html"><div class="logo"></div></a>
+    <a href="../index.html"><div class="logo" aria-hidden="true"></div><span class="sr-only">Logo</span></a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
             aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
@@ -89,7 +89,7 @@
                         </div>
 
                         <div class="col">
-                            <label type="submit" class="numPer numPerPad" id="number"></label>
+                            <label type="submit" class="numPer numPerPad" id="number" alt="Numero adult"></label>
                         </div>
 
                         <div class="col-1">
@@ -97,7 +97,7 @@
                         </div>
 
                         <div class="col">
-                            <label type="submit" class="numPer numPerPad" id="number"></label>
+                            <label type="submit" class="numPer numPerPad" id="number" alt="Numero niño"></label>
                         </div>
                     </div>
                 </div>
@@ -116,7 +116,7 @@
                         </div>
 
                         <div class="col">
-                            <label type="submit" class="numPer numPerPad" id="number"></label>
+                            <label type="submit" class="numPer numPerPad" id="number" alt="Numero adult"></label>
                         </div>
 
                         <div class="col-1">
@@ -124,7 +124,7 @@
                         </div>
 
                         <div class="col">
-                            <label type="submit" class="numPer numPerPad"  id="number"></label>
+                            <label type="submit" class="numPer numPerPad"  id="number" alt="Numero niño"></label>
                         </div>
                     </div>
                 </div>
@@ -165,10 +165,9 @@
 
             <div class="socials">
                 <ul class="footerNavigator">
-                    <li class="footerItem"><a class="footerLink" href="#"><i class="fab fa-facebook-square"></i></a>
-                    </li>
-                    <li class="footerItem"><a class="footerLink" href="#"><i class="fab fa-instagram"></i></a></li>
-                    <li class="footerItem"><a class="footerLink" href="#"><i class="fab fa-twitter"></i></a></li>
+                <li class="footerItem"><a class="footerLink" href="#"><i class="fab fa-facebook-square" aria-hidden="true"></i><span class="sr-only">Facebook</span></a></li>
+                    <li class="footerItem"><a class="footerLink" href="#"><i class="fab fa-instagram" aria-hidden="true"></i><span class="sr-only">Instagram</span></a></li>
+                    <li class="footerItem"><a class="footerLink" href="#"><i class="fab fa-twitter" aria-hidden="true"></i><span class="sr-only">Twitter</span></a></li>
                 </ul>
             </div>
             <p class="footertext"> copyright &copy;2021 TropycalBlast. </p>
