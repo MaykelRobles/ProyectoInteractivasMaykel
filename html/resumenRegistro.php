2e27005d9ed787288945c8274a6bc0ea627e783d
MaykelRobles <82823403+MaykelRobles@users.noreply.github.com>
2021-06-07 18:50:52 -0600

    cambio a php
    de html a php

--- html/resumenRegistro.php
+++ html/resumenRegistro.php
@@ -20,7 +20,7 @@
 
 <nav class="navbar navbar-expand-lg navColor">
     <div class="container-fluid">
-        <a href="../index.html"><div class="logo" aria-hidden="true"></div><span class="sr-only">Logo</span></a>
+        <a href="../index.php"><div class="logo" aria-hidden="true"></div><span class="sr-only">Logo</span></a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
             aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
@@ -28,10 +28,10 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item">
-                    <a class="nav-link" href="../index.html">Home</a>
+                    <a class="nav-link" href="../index.php">Home</a>
                 </li>
                 <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="../html/detail.html" id="navbarDropdown" role="button"
+                    <a class="nav-link dropdown-toggle" href="../html/detail.php" id="navbarDropdown" role="button"
                         data-bs-toggle="dropdown" aria-expanded="false">
                         Categorias
                     </a>
