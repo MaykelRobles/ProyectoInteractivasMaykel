d6248cec1a6fa2cf47c8bd383915778bd130375c
MaykelRobles <82823403+MaykelRobles@users.noreply.github.com>
2021-06-07 18:49:20 -0600

    cambio a php
    de html a php

--- html/detail.php
+++ html/detail.php
@@ -20,7 +20,7 @@
 
     <nav class="navbar navbar-expand-lg navColor">
         <div class="container-fluid">
-            <a href="../index.html"><div class="logo" aria-hidden="true"></div><span class="sr-only">Logo</span></a>
+            <a href="../index.php"><div class="logo" aria-hidden="true"></div><span class="sr-only">Logo</span></a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
                 data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
                 aria-label="Toggle navigation">
@@ -29,10 +29,10 @@
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-                        <a class="nav-link" href="../index.html">Home</a>
+                        <a class="nav-link" href="../index.php">Home</a>
                     </li>
                     <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="../html/detail.html" id="navbarDropdown" role="button"
+                        <a class="nav-link dropdown-toggle" href="../html/detail.php" id="navbarDropdown" role="button"
                             data-bs-toggle="dropdown" aria-expanded="false">
                             Categorias
                         </a>
@@ -97,20 +97,20 @@
         </div>
 
         <div class="containerBttnRegistro">
-            <a class="col-5" href="../html/registrarse.html">
+            <a class="col-5" href="../html/registrarse.php">
                 <div class="botonRegistrarse">
                     <p class="textBoton">REGISTRARSE</p>
                 </div>
             </a>
 
             <div class="row">
-                <a class="col" href="../html/detail.html">
+                <a class="col" href="../html/detail.php">
                     <div class="containerEventosRelacionados">
                         <div class="circulo"></div>
                     </div>
                     <p class="footertext">Evento Relacionado</p>
                 </a>
-                <a class="col" href="../html/detail.html">
+                <a class="col" href="../html/detail.php">
                     <div class="containerEventosRelacionados">
                         <div class="circulo"></div>
                     </div>
